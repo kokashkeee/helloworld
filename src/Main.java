1c9dc6b153b6d3a180067990e60c3facbcef6620
New <kravchenko.2455@gmail.com>
2025-05-29 18:32:19 +0700

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
@@ -7,6 +5,5 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
-
     }
 }
