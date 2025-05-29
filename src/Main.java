a90b0856e263d5e54790d7249249997df558c020
New <kravchenko.2455@gmail.com>
2025-05-29 18:26:59 +0700

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!\n");
+        System.out.println("Hello, world!");
+        System.out.printf("Привет, мир!\n");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+
     }
 }
